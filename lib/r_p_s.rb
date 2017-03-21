cc7345028e29d3b1684a94f1c677ef878a823867
Seve Sheldon <sevesheldon@gmail.com>
2017-03-21 13:13:35 -0700

    add the next seven working specs for beats?().

--- lib/r_p_s.rb
+++ lib/r_p_s.rb
@@ -8,6 +8,28 @@ class String
         true
       elsif opponent_turn == "paper"
         false
+      elsif opponent_turn == "rock"
+        "tie"
+      end
+
+    elsif turn == "scissors"
+
+      if opponent_turn == "paper"
+        true
+      elsif opponent_turn == "rock"
+        false
+      elsif opponent_turn == "scissors"
+        "tie"
+      end
+
+    elsif turn == "paper"
+
+      if opponent_turn == "scissors"
+        true
+      elsif opponent_turn == "rock"
+        false
+      elsif opponent_turn == "paper"
+        "tie"  
       end
 
     end
